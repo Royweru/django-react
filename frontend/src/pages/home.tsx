2cb9966e58ab2a44efde5861e7a6d910d81f0691
Royweru <royezi17@gmail.com>
2024-06-06 07:13:32 +0300

    modified:   CoolNotes/api/__pycache__/serializers.cpython-311.pyc 	modified:   CoolNotes/api/__pycache__/views.cpython-311.pyc 	modified:   CoolNotes/db.sqlite3 	modified:   frontend/src/pages/home.tsx

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../api";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  useEffect(() => {
+    getNote();
+  }, []);
+  const getNote = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+  const deleteNote = (id: string) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Note deleted");
+        else {
+          alert("Failed to delete note");
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+  const createNote = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    api.post("/api/notes/", { content, title }).then((res) => {
+      if (res.status === 201) alert("Note created");
+      else alert("Failed to create Note");
+    });
+  };
+  return (
+    <div>
+      <div>
+        <h2>Notes</h2>
+      </div>
+      <form action="" onSubmit={createNote}>
+        <label htmlFor="title">Title</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="contnet">Content</label>
+        <br />
+        <textarea
+          name="content"
+          id="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <br />
+        <input type="submit" value="SUBMIT" />
+      </form>
+    </div>
+  );
 };
